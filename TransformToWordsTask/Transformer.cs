018cfdabaccfa43cb05102b8748dbd81de76e37e
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-30 10:47:47 +0300

    add-task-and-test-projects

---
+++ TransformToWordsTask/Transformer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TransformToWordsTask
+{
+    /// <summary>
+    /// Provides transformer methods.
+    /// </summary>
+    public class Transformer
+    {
+        /// <summary>
+        /// Converts number's digital representation into words.
+        /// </summary>
+        /// <param name="number">Number to convert.</param>
+        /// <returns>Words representation.</returns>
+        public string TransformToWords(double number)
+        {
+            throw  new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
