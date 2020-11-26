2b29a867d2675aaefd208c5772d61db304560319
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-27 02:10:22 +0300

    Up

--- TransformToWordsTask/Transformer.cs
+++ TransformToWordsTask/Transformer.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Text;
 
 #pragma warning disable CA1822
 
@@ -16,7 +17,89 @@ namespace TransformToWordsTask
         /// <returns>Words representation.</returns>
         public string TransformToWords(double number)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (number is double.Epsilon)
+            {
+                return "Double Epsilon";
+            }
+
+            if (double.IsNaN(number))
+            {
+                return "NaN";
+            }
+
+            if (number == double.NegativeInfinity)
+            {
+                return "Negative Infinity";
+            }
+
+            if (number == double.PositiveInfinity)
+            {
+                return "Positive Infinity";
+            }
+
+            string result = "";
+
+            char[] n = number.ToString().ToCharArray();
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                switch (n[i])
+                {
+                    case '0':
+                        result += "zero";
+                        break;
+                    case '1':
+                        result += "one";
+                        break;
+                    case '2':
+                        result += "two";
+                        break;
+                    case '3':
+                        result += "three";
+                        break;
+                    case '4':
+                        result += "four";
+                        break;
+                    case '5':
+                        result += "five";
+                        break;
+                    case '6':
+                        result += "six";
+                        break;
+                    case '7':
+                        result += "seven";
+                        break;
+                    case '8':
+                        result += "eight";
+                        break;
+                    case '9':
+                        result += "nine";
+                        break;
+                    case '+':
+                        result += "plus";
+                        break;
+                    case '-':
+                        result += "minus";
+                        break;
+                    case 'E':
+                        result += "E";
+                        break;
+                    case ',':
+                        result += "point";
+                        break;
+                }
+
+                if (i < n.Length - 1)
+                {
+                    result += " ";
+                }
+            }
+
+            var sb = new StringBuilder(result);
+            sb[0] = char.ToUpper(sb[0]);
+            result = sb.ToString();
+
+            return result;
         }
     }
 }
